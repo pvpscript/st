83d9c371fc8885e90983645804ea2f0e061deb91
Pedro Vitor Piassa <pvpiassa@gmail.com>
2020-03-28 16:25:12 -0300

    Added 'font2' patch

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Original: [st](https://st.suckless.org/)
 
 This build contains the following patches:
   * [scrollback](https://st.suckless.org/patches/scrollback/) 
+  * [font2](https://st.suckless.org/patches/font2/)
 
 Including some changes in the config file.
--- config.h
+++ config.h
@@ -6,6 +6,12 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+	"DejaVu Sans Mono:pixelsize=12:antialias=true:autohint=true",
+	"Inconsolata:pixelsize=11:antialias=true:autohint=true",
+};
+
 static int borderpx = 2;
 
 /*
